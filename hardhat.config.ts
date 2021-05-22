9048e5417d80d6d9bf29ddfb112716117d4f8b2f
Weiwu Zhang <a@colourful.land>
2021-05-22 21:22:43 +1000

    creating the deployment script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,11 @@
 import { task } from 'hardhat/config';
 import "@nomiclabs/hardhat-waffle";
 
+/* Deploy using alchemy, Go to https://www.alchemyapi.io, sign up,
+   create a new App in its dashboard, and replace "KEY" with its
+   key */
+const { PRIVATE_KEY, ALCHEMY_API_KEY } = process.env;
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (args, hre) => {
@@ -10,6 +15,32 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
     console.log(account.address);
   }
 });
+
+module.exports = {
+  solidity: "0.8.0",
+  networks: {
+    hardhat: {
+      accounts: [
+        {
+          balance: "21408160000000000", // balance in WEI
+          privateKey: `${PRIVATE_KEY}`
+        },
+      ]
+    },
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`${PRIVATE_KEY}`]
+    },
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`${PRIVATE_KEY}`]
+    },
+    mainnet: {
+      url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`${PRIVATE_KEY}`]
+    }
+  }
+};
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
